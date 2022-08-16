6ca7a4ad68c82615d42ed5438e6ff3d36a8a6760
unknown <suntechnic@mail.ru>
2022-08-16 12:30:44 +0400

    make stepik final prj

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as Options_Chrome
+#from selenium.webdriver.chrome.options import Options as Options_FireFox
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: ru, en, es, ... etc")  # +++
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language") # +++
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        chrome_options = Options_Chrome()
+        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=chrome_options)
+    elif browser_name == "firefox":
+        print("\nStart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)        
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nQuit browser..")
+    browser.quit()
